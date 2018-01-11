7a20f5b37726591992f87ad8a556ffde3e687e6f
altarit <mail@altarit.com>
2018-01-12 01:56:46 +0300

    moved to prop-types

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Content from '../Content'
 import Sidebar from '../../components/SideBar'
@@ -15,7 +16,7 @@ export const CoreLayout = ({children}) => (
 )
 
 CoreLayout.propTypes = {
-  children: React.PropTypes.element.isRequired
+  children: PropTypes.element.isRequired
 }
 
 export default CoreLayout
